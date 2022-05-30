93c1179a4a63b910f6fde577872662c0b1415451
RitaYang <815240067@qq.com>
2022-05-30 20:23:48 +0800

    完成了index.html部分

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,6 +1,22 @@
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter ((option)=>{
-    option.url="http://www.liulongbin.top:3007"+option.url
+$.ajaxPrefilter((option) => {
+  option.url = "http://www.liulongbin.top:3007" + option.url
+  if (option.url.includes("/my/")) {
+    option.headers = {
+      Authorization: localStorage.getItem("token"),
+    }
+  }
+  //   拦截请求，不管成功失败complete都会执行
+  option.complete = (res) => {
+    //   console.log(res);
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === "身份认证失败！"
+    ) {
+      localStorage.removeItem("token")
+      location.href = "/login.html"
+    }
+  }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+function getUserInfo() {
+  $.ajax({
+    type: "GET",
+    url: "/my/userinfo",
+    // headers: { Authorization: localStorage.getItem("token") },
+    success: (res) => {
+      if (res.status !== 0) return layer.msg(res.message)
+      layer.msg(res.message)
+      console.log(res)
+      renderAvatar(res.data)
+    },
+  })
+}
+const renderAvatar = (user) => {
+  // console.log(user);
+  //设置欢迎语
+  const uname = user.nickname || user.username
+  $("#welcome").html(`欢迎 ${uname}`)
+  //按需渲染头像
+  if (user.user_pic) {
+    $(".layui-nav-img").attr("src", user.user_pic)
+    $(".text-avatar").hide()
+  } else {
+    $(".layui-nav-img").hide()
+    $(".text-avatar").html(uname[0].toUpperCase())
+  }
+  $("#btnquit").on("click", function () {
+    layui.layer.confirm(
+      "确定退出登录？",
+      { icon: 3, title: "" },
+      function (index) {
+        layer.close(index)
+        // 清空本地存储里面的 token
+        localStorage.removeItem("token")
+        // 重新跳转到登录页面
+        location.href = "/login.html"
+      }
+    )
+  })
+}
+getUserInfo()
